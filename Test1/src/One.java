befdd35dd48912e36570ae186533aef5da173611
joeldo04 <joeladominguez04@gmail.com>
2025-11-28 12:11:34 -0500

    Rama 2

--- Test1/src/One.java
+++ Test1/src/One.java
@@ -1,5 +1,12 @@
 public class One {
     public static void main(String[] args) {
         System.out.println("Rama 1");
+        
+        int num1 = 1;
+        int num2 = 10;
+        
+        int suma = num1 + num2;
+        
+        System.out.println("Suma " + suma);
     }
 }
