a6e60cce3728cc5389e1679548207b66fceaa6dd
joeldo04 <joeladominguez04@gmail.com>
2025-11-28 12:15:48 -0500

    Rama 1 Main cambios

--- Test1/src/One.java
+++ Test1/src/One.java
@@ -1,6 +1,11 @@
 
 public class One {
     public static void main(String[] args) {
-        System.out.println("Rama 1");
+        System.out.println("Rama 1 - Main");
+        
+        int num1 = 100;
+        int num2 = 50;
+        int resta = num1 - num2;
+        System.out.println("Rama main prueba resta " + resta);
     }
 }
